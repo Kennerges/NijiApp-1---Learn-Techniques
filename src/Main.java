d2af83c4ee7f8a41d759693539fe627f96d5b8a0
Kennerges <kennerges@gmail.com>
2020-08-11 15:12:42 -0300

    Added Calculator of Hours and Scenes Without Masteries

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How much different values this technique has? ");
+        int numberValues = sc.nextInt();
+        double[] valuesTechnique = new double[numberValues];
+        System.out.println("Insert the values of the technique: ");
+        for (int i = 0; i < numberValues; i ++) {
+            valuesTechnique[i] = sc.nextInt();
+        }
+        System.out.println("Insert the value's attributes of the character, in sequence: ");
+        double[] valuesCharacter = new double[numberValues];
+        for (int i = 0; i < numberValues; i ++) {
+            valuesCharacter[i] = sc.nextInt();
+        }
+
+        System.out.println("Insert the value of the Wisdom of your character: ");
+        double wisdom = sc.nextInt();
+
+        double hours = 0;
+        double accumulatedPoints = 0;
+
+        for (int i = 0; i < numberValues; i ++) {
+            hours = hours + valuesTechnique[i] / valuesCharacter[i] * valuesTechnique[i] / 50;
+            accumulatedPoints = accumulatedPoints + valuesTechnique[i];
+        }
+
+        double scenes = accumulatedPoints / wisdom * accumulatedPoints / 50;
+
+        System.out.println(hours);
+        System.out.println(scenes);
+
+        sc.close();
 
     }
 }
